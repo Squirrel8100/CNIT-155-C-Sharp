8d7004daf53fc77de5594f5f3215a23f1af50465
Logan Fish <92543826+Squirrel8100@users.noreply.github.com>
2022-12-17 00:12:21 -0500

    Add files via upload

---
+++ Debugging1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace HwangJay_InLab04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.cboTopping = new System.Windows.Forms.ComboBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOrderDisplay = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Count: (# of Pizzas)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Toppings:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // txtCount
+            // 
+            this.txtCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCount.Location = new System.Drawing.Point(182, 57);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(143, 20);
+            this.txtCount.TabIndex = 3;
+            // 
+            // cboTopping
+            // 
+            this.cboTopping.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTopping.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboTopping.FormattingEnabled = true;
+            this.cboTopping.Items.AddRange(new object[] {
+            "Meat Lovers",
+            "Pepperoni",
+            "Mushroom",
+            "Sausage",
+            "Veggie",
+            "Plain"});
+            this.cboTopping.Location = new System.Drawing.Point(182, 98);
+            this.cboTopping.Name = "cboTopping";
+            this.cboTopping.Size = new System.Drawing.Size(143, 21);
+            this.cboTopping.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(36, 161);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(350, 121);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompute.Location = new System.Drawing.Point(434, 161);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(132, 37);
+            this.btnCompute.TabIndex = 6;
+            this.btnCompute.Text = "&Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.BtnCompute_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(434, 204);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(132, 37);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(434, 247);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(132, 35);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(412, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(320, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "# of Orders:";
+            // 
+            // txtOrderDisplay
+            // 
+            this.txtOrderDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOrderDisplay.Location = new System.Drawing.Point(434, 304);
+            this.txtOrderDisplay.Multiline = true;
+            this.txtOrderDisplay.Name = "txtOrderDisplay";
+            this.txtOrderDisplay.ReadOnly = true;
+            this.txtOrderDisplay.Size = new System.Drawing.Size(132, 28);
+            this.txtOrderDisplay.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 363);
+            this.Controls.Add(this.txtOrderDisplay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.cboTopping);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InLab04 By Jay";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.ComboBox cboTopping;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOrderDisplay;
+    }
+}
+
---
+++ Debugging1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Debugging exercise 
+ =======================================================================
+ Your Name : Logan Fish
+ Email:fishl@purdue.edu
+ Program Description: This is the solution for inlab-4 with some Build errors injected.
+        
+           There are about 10 bugs.
+           Write a comment after fixing each bug after the line // ....
+           Once done, zip the working project and submit to the link.
+ ==========================================================================
+ */
+
+namespace HwangJay_InLab04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Counter 
+        //private int mCount = 0;
+        //price for plain pizza (starting price)
+        //private const double cPrice = 6.00;
+
+        private int mCount = 0;        //Count the orders
+        // Named Constants
+        private const double cPrice = 6.00;     //price for plain pizza (starting price)
+        private const double cTopPepperoni = 2.0;
+        private const double cTopMeat = 4.50;
+        private const double cTopSausage = 2.50;
+        private const double cTopMushroom = 1.0;
+        private const double cTopVeggie = 3.50;
+
+       
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtCount.Clear();
+            txtOrderDisplay.Clear();
+            txtOutput.Clear();
+            cboTopping.SelectedIndex = -1;
+            txtCount.Focus();
+        }
+
+
+        private void BtnCompute_Click(object sender, EventArgs e)
+        {
+            int numofpizza;
+            string toppings;
+            double totalPrice;
+
+            totalPrice = 0;
+            numofpizza = int.Parse(txtCount.Text); // Try parse should be int.parse
+            toppings = cboTopping.Text;
+
+            mCount++;      // one more order is processed
+
+            txtOrderDisplay.Text = mCount.ToString();
+
+
+            if (cboTopping.Text == "Meat Lovers")  //Meat Lovers // added another "=" into the statement as it needs two
+            {
+                totalPrice = numofpizza * (cPrice + 4.50);
+            }
+
+            else if (cboTopping.Text == "Pepperoni")   //Pepperoni         // removed the ; because the statement shouldnt have it
+            {
+                totalPrice = numofpizza * (cPrice + 2.00);
+
+            }
+            else if (cboTopping.Text == "Mushroom")   //Mushroom  //added if into the statement 
+            {
+                totalPrice = numofpizza * (cPrice + 1.00);
+            }
+            else if (cboTopping.Text == "Sausage") // added if into the statement
+            {
+                totalPrice = numofpizza * (cPrice + cTopSausage);
+            }
+            else if (cboTopping.Text == "Veggie")    //Veggie // added if into the statement
+            {
+                totalPrice = numofpizza * (cPrice + 3.50);
+
+            }
+            else    //Plain
+            {
+                totalPrice = cPrice * numofpizza;
+            }
+
+            txtOutput.Text = "Order Summary : " + "\r\n" + numofpizza.ToString() + " Pizza (s)"
+                    + "\r\nType of Pizza : " + toppings + "\r\nDue Amount: " + totalPrice.ToString("c");
+
+            if (mCount == 10) // added another = to make the statement work, also removed the ; at the end as it shouldnt have it. 
+            {
+                txtOutput.Text = txtOutput.Text + "\r\n\r\nCongraturations!!!!";
+            }
+
+        }
+        
+    }
+}
