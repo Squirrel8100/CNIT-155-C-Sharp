0943d068a29396b6267fed12ecbacd55ee213bf4
Logan Fish <92543826+Squirrel8100@users.noreply.github.com>
2022-12-17 00:07:05 -0500

    Add files via upload

---
+++ InLab5/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace InLab05FishLogan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtStemsNum = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblStems = new System.Windows.Forms.Label();
+            this.lblValentineRose = new System.Windows.Forms.Label();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.chkWrap = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(206, 144);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtStemsNum
+            // 
+            this.txtStemsNum.Location = new System.Drawing.Point(206, 188);
+            this.txtStemsNum.Name = "txtStemsNum";
+            this.txtStemsNum.Size = new System.Drawing.Size(149, 20);
+            this.txtStemsNum.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(108, 147);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name:";
+            // 
+            // lblStems
+            // 
+            this.lblStems.AutoSize = true;
+            this.lblStems.Location = new System.Drawing.Point(108, 195);
+            this.lblStems.Name = "lblStems";
+            this.lblStems.Size = new System.Drawing.Size(58, 13);
+            this.lblStems.TabIndex = 4;
+            this.lblStems.Text = "# of Stems";
+            // 
+            // lblValentineRose
+            // 
+            this.lblValentineRose.AutoSize = true;
+            this.lblValentineRose.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValentineRose.Location = new System.Drawing.Point(201, 36);
+            this.lblValentineRose.Name = "lblValentineRose";
+            this.lblValentineRose.Size = new System.Drawing.Size(148, 28);
+            this.lblValentineRose.TabIndex = 5;
+            this.lblValentineRose.Text = "Valentine Roses";
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(91, 412);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(75, 23);
+            this.btnCompute.TabIndex = 6;
+            this.btnCompute.Text = "&Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.BtnCompute_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(186, 412);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(280, 412);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(91, 259);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(264, 135);
+            this.txtOutput.TabIndex = 9;
+            // 
+            // chkWrap
+            // 
+            this.chkWrap.AutoSize = true;
+            this.chkWrap.Location = new System.Drawing.Point(206, 224);
+            this.chkWrap.Name = "chkWrap";
+            this.chkWrap.Size = new System.Drawing.Size(77, 17);
+            this.chkWrap.TabIndex = 10;
+            this.chkWrap.Text = "Gift Wrap?";
+            this.chkWrap.UseVisualStyleBackColor = true;
+            this.chkWrap.CheckedChanged += new System.EventHandler(this.ChkWrap_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 564);
+            this.Controls.Add(this.chkWrap);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.lblValentineRose);
+            this.Controls.Add(this.lblStems);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtStemsNum);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "InLab05 Data Visualization By Logan Fish";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtStemsNum;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblStems;
+        private System.Windows.Forms.Label lblValentineRose;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.CheckBox chkWrap;
+    }
+}
+
---
+++ InLab5/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*Logan Fish   
+ * Email:Fishl@purdue.edu
+ * Cnit155 inLab-05
+ */
+namespace InLab05FishLogan
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const Double OneRose = 5.0;
+        private const Double MultiRoseUnitPrice = 4.0;
+        private const Double GiftWrap = 3.0;
+        private const Double TaxRate = 0.08;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCompute_Click(object sender, EventArgs e)
+        {
+            
+
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Please enter a Name", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtName.Focus();
+                return; // return
+            }
+
+
+            int Stems = 0;
+
+            if (int.TryParse(txtStemsNum.Text, out Stems) == false)
+            {
+                MessageBox.Show("Number of stems must be a whole number", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtStemsNum.Focus();
+                return; // return
+            }
+
+            if (Stems < 1 || Stems >12)
+            {
+                MessageBox.Show("Number of roses should be between 1-12", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtStemsNum.Focus();
+                return; // return
+            }
+
+
+
+
+            int Roses;
+            Roses = int.Parse(txtStemsNum.Text);
+
+            Double PriceSingle; //price of single rose
+            PriceSingle = OneRose;
+
+            Double MoreRoses; //price of more than one rose
+            MoreRoses = MultiRoseUnitPrice * Roses;
+
+            Double SingleTax; // price of single rose with tax
+            SingleTax = (PriceSingle * TaxRate) + PriceSingle;
+
+            
+            Double MultiTax = (MoreRoses * TaxRate) + MoreRoses; //price of more roses taxed
+
+            Double Wrapped;
+            Wrapped = PriceSingle + GiftWrap;   //price of single rose with gift wrap
+
+            Double WrappedTaxed;
+            WrappedTaxed = (Wrapped * TaxRate) + Wrapped; // price of single rose with gift wrapped taxed
+
+            Double MoreWrapped;
+            MoreWrapped = (MoreRoses + GiftWrap); // price of more roses with wrap
+
+            Double MoreWrappedTax;
+            MoreWrappedTax = (MoreWrapped * TaxRate) + MoreWrapped; // price of more roses with wrap taxed. 
+
+
+
+            if (Roses == 1)
+            {
+               txtOutput.Text = "Customer: " + txtName.Text + "\r\n" + "Number of Roses " + Roses + "\r\n"+
+                    "Price" + PriceSingle.ToString("C") + "\r\n" + "Price after tax: " + SingleTax;
+                if (chkWrap.Checked == true)
+                {
+                    txtOutput.Text = "Customer: " + txtName.Text + "\r\n" + "Number of Roses " + Roses
+                        + "\r\n" +
+                    "Price" + Wrapped.ToString("C") + "\r\n" + "Price after tax: " + WrappedTaxed.ToString("C");
+                }
+            }
+            
+            else if (Roses >=2)
+            {
+                txtOutput.Text = "Customer: " + txtName.Text + "\r\n" + "Number of Roses " + Roses + "\r\n" +
+                   "Price" + MoreRoses.ToString("C") + "\r\n" + "Price after tax: " + MultiTax.ToString("C");
+                if (chkWrap.Checked == true)
+                {
+                    txtOutput.Text = "Customer: " + txtName.Text + "\r\n" + "Number of Roses " + Roses
+                       + "\r\n" +
+                   "Price" + MoreWrapped.ToString("C") + "\r\n" + "Price after tax: " + MoreWrappedTax.ToString("C");
+                }
+            }
+
+        }
+
+        private void ChkWrap_CheckedChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            chkWrap.Checked = false;
+            txtName.Text = "";
+            txtOutput.Text = "";
+            txtStemsNum.Text = "";
+            txtName.Focus();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
